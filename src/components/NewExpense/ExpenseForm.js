f093bca00c054799df6f354fc787dd9366690f21
unknown <bdrozdek@gmail.com>
2023-08-23 14:46:07 +0200

    update

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -14,27 +14,49 @@ const ExpenseForm = () => {
 	const dateChangeHandler = event => {
 		setEnteredDate(event.target.value)
 	}
+	const submitHandler = e => {
+		e.preventDefault()
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate),
+		}
+		setEnteredTitle('')
+		setEnteredAmount('')
+		setEnteredDate('')
+	}
 
 	return (
-		<form>
-			<div className="new-expense__controls">
-				<div className="new-expense__control">
+		<form onSubmit={submitHandler}>
+			<div className='new-expense__controls'>
+				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input type='text' value={enteredTitle} onChange={titleChangeHandler} />
 				</div>
-				<div className="new-expense__control">
+				<div className='new-expense__control'>
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+					<input type='number' value={enteredAmount} min='0.01' step='0.01' onChange={amountChangeHandler} />
 				</div>
-				<div className="new-expense__control">
+				<div className='new-expense__control'>
 					<label>Date</label>
-					<input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+					<input type='date' value={enteredDate} min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
 				</div>
 			</div>
-			<div className="new-expense__actions">
-				<button type="submit">Add Expense</button>
+			<div className='new-expense__actions'>
+				<button type='submit'>Add Expense</button>
 			</div>
 		</form>
 	)
 }
 export default ExpenseForm
+
+// const [userInput, setUserInput] = useState({ enteredTitle: '', enteredAmount: '', enteredDate: '' })
+
+// const titleChangeHandler = e => {
+// 	setUserInput(prevState => {
+// 		return {
+// 			...prevState,
+// 			enteredTitle: e.target.value,
+// 		}
+// 	})
+// }
